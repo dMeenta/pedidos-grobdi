7376b3be4741c11d3942a8cce9a8d780a102f2f7
GrobdiDevOwO <programador.grobdi@gmail.com>
2025-09-09 09:01:32 -0500

    Fix: Pedidos_delivery_state compatibility fixed

--- database/migrations/2025_09_04_130054_create_pedidos_delivery_state_table.php
+++ database/migrations/2025_09_04_130054_create_pedidos_delivery_state_table.php
@@ -18,10 +18,8 @@ return new class extends Migration
             $table->foreignId('motorizado_id')->nullable()->constrained('users');
             $table->text('observacion')->nullable();
             $table->string('foto_domicilio')->nullable();
-            $table->timestamp('datetime_foto_domicilio')->nullable();
             $table->string('foto_entrega')->nullable();
-            $table->timestamp('datetime_foto_entrega')->nullable();
-            $table->timestamp('created_at')->useCurrent();
+            $table->timestamps();
         });
     }
 
---
+++ database/migrations/2025_09_09_084725_add_receptor_data_to_pedidos_delivery_state_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('pedidos_delivery_state', function (Blueprint $table) {
+            $table->string('receptor_nombre')->nullable()->after('datetime_foto_entrega');
+            $table->longText('receptor_firma')->nullable()->after('receptor_nombre');
+        });
+    }
+    /**
+     * * Reverse the migrations.
+     * */
+
+    public function down(): void
+    {
+        Schema::table('pedidos_delivery_state', function (Blueprint $table) {
+            $table->dropColumn(['receptor_nombre', 'receptor_firma']);
+        });
+    }
+};
