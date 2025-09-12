64ec67f8966f7e13f1173ef697c8a4c904186dc4
sastod1996 <saulastodiaz@gmail.com>
2025-09-12 16:46:32 -0500

    cambio responsive asignar pedidos

--- resources/views/pedidos/counter/asignar_pedido/index.blade.php
+++ resources/views/pedidos/counter/asignar_pedido/index.blade.php
@@ -1,6 +1,6 @@
 @extends('adminlte::page')
 
-@section('title', 'Dashboard')
+@section('title', 'Asignar Pedidos')
 @section('content')
 <div class="card mt-5">
     <h2 class="card-header">Pedidos</h2>
@@ -26,55 +26,56 @@
             @foreach($zonas as $zona)
                 <div class="col-xs-6 col-sm-6 col-md-6">
                     <label for="fecha_inicio">{{ $zona->name }}</label>
-
-                    <table class="table table-bordered table-striped mt-4">
-                        <thead>
-                            <tr>
-                                <th>Nro</th>
-                                <th>Nro pedido</th>
-                                <th>Fecha creada</th>
-                                <th>Distrito</th>
-                                <th>Zonas</th>
-                                <th width="120px">Opciones</th>
-                            </tr>
-                        </thead>
-            
-                        <tbody>
-                        @forelse ($pedidos as $pedido)
-                            @if ($pedido->zone_id == $zona->id)
-                            <tr>
-                                <td>{{ $pedido->nroOrder }}</td>
-                                <td>{{ $pedido->orderId }}</td>
-                                <td>{{ $pedido->created_at }}  </td>
-                                <td>{{ $pedido->district}}</td>
-                                <form action="{{ route('asignarpedidos.update',$pedido->id) }}" method="POST">
-                                @csrf
-                                @method('PUT')
-                                <td>
-                                    <select class="form-select form-select-sm" aria-label=".form-select-sm example"  name="zone_id" id="zone_id">
-                                        <option disabled>Cambiar zona</option>
-                                        @foreach ($zonas as $zon)
-                                            <option value={{ $zon->id }} {{ $pedido->zone_id ===  $zon->id  ? 'selected' : '' }}>{{$zon->name}}</option>
-                                        
-                                        @endforeach
-                                    </select>
-                                </td>
-                                <td>
-                                    <!-- <a class="btn btn-success btn-sm" data-toggle="modal" data-target="#createModal" href="{{ route('asignarpedidos.show',$pedido->id) }}"><i class="fa fa-eye"></i> ver</a> -->
-                        
-                                    <button type="submit" class="btn btn-primary btn-sm"><i class="fa fa-pencil-square"></i> cambiar</button>
-                                </td>
-                                </form>
-                            </tr>
-                            @endif
-                        @empty
-                            <tr>
-                                <td colspan="6">No hay información que mostrar</td>
-                            </tr>
-                        @endforelse
-                        </tbody>
-            
-                    </table>
+                    <div class="table table-responsive">
+                        <table class="table table-bordered table-striped mt-4">
+                            <thead>
+                                <tr>
+                                    <th>Nro</th>
+                                    <th>Nro pedido</th>
+                                    <th>Fecha creada</th>
+                                    <th>Distrito</th>
+                                    <th>Zonas</th>
+                                    <th width="120px">Opciones</th>
+                                </tr>
+                            </thead>
+                
+                            <tbody>
+                            @forelse ($pedidos as $pedido)
+                                @if ($pedido->zone_id == $zona->id)
+                                <tr>
+                                    <td>{{ $pedido->nroOrder }}</td>
+                                    <td>{{ $pedido->orderId }}</td>
+                                    <td>{{ $pedido->created_at }}  </td>
+                                    <td>{{ $pedido->district}}</td>
+                                    <form action="{{ route('asignarpedidos.update',$pedido->id) }}" method="POST">
+                                    @csrf
+                                    @method('PUT')
+                                    <td>
+                                        <select class="form-select form-select-sm" aria-label=".form-select-sm example"  name="zone_id" id="zone_id">
+                                            <option disabled>Cambiar zona</option>
+                                            @foreach ($zonas as $zon)
+                                                <option value={{ $zon->id }} {{ $pedido->zone_id ===  $zon->id  ? 'selected' : '' }}>{{$zon->name}}</option>
+                                            
+                                            @endforeach
+                                        </select>
+                                    </td>
+                                    <td>
+                                        <!-- <a class="btn btn-success btn-sm" data-toggle="modal" data-target="#createModal" href="{{ route('asignarpedidos.show',$pedido->id) }}"><i class="fa fa-eye"></i> ver</a> -->
+                            
+                                        <button type="submit" class="btn btn-primary btn-sm"><i class="fa fa-pencil-square"></i> cambiar</button>
+                                    </td>
+                                    </form>
+                                </tr>
+                                @endif
+                            @empty
+                                <tr>
+                                    <td colspan="6">No hay información que mostrar</td>
+                                </tr>
+                            @endforelse
+                            </tbody>
+                
+                        </table>
+                    </div>
                 </div>
 
             @endforeach
