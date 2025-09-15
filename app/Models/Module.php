27daafccaa068397990e199093f1fba322e6f9e9
sastod1996 <saulastodiaz@gmail.com>
2025-09-15 09:19:13 -0500

    se creo la tabla roles_views y se agregó las relaciones al modelo

--- app/Models/Module.php
+++ app/Models/Module.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Module extends Model
 {
-    //
+    public function modules() {
+        return $this->belongsToMany(Module::class, 'roles_modules');
+    }
+    public function views() {
+        return $this->belongsToMany(View::class, 'roles_views');
+    }
 }
