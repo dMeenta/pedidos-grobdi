34912a869f1ab535b4b73ea3f210339881a1f666
GrobdiDevOwO <programador.grobdi@gmail.com>
2025-10-07 08:15:20 -0500

    Feat: Reports Repository Interface + Impl

---
+++ app/Domain/Interfaces/ReportsRepositoryInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Domain\Interfaces;
+
+use Illuminate\Support\Collection;
+
+interface ReportsRepositoryInterface
+{
+    public function getVentasGeneralReport(int $month, int $year): Collection;
+    public function getVentasVisitadorasReport(string $startDate, string $endDate): array;
+    public function getVentasProductosReport(string $startDate, string $endDate): array;
+    public function getVentasProvinciasReport(array $filters = []): array;
+    public function getRutasZonesReport(array $filters = []): array;
+    public function getAmountSpentAnuallyByDoctor(int $year, int $doctorId): array;
+    public function getMostConsumedProductsMonthlyByDoctor(int $year, int $month, int $doctorId): Collection;
+    public function getAmountSpentMonthlyGroupedByTipo(int $year, int $month, int $doctorId): Collection;
+    public function getTopDoctorByAmountInfo(int $year): mixed;
+    public function getDoctorInfo(int $doctorId): mixed;
+    public function getRawDataGeoVentas(string $startDate, string $endDate): Collection;
+    public function getRawDataGeoVentasDetails(string $startDate, string $endDate): Collection;
+    public function getDepartamentosForMap(): array;
+    public function getProvinciasForMap(): array;
+    public function getProvinciasWithDepartamentoForMap(): array;
+    public function getDistritosWithProvinciaAndDepartamentoForMap(): array;
+    public function getDistritosWithProvinciaForMap(): array;
+}
